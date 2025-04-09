74135dbd91a19e0fbf148a360d7c872496c8daeb
yudi-star <yudith.pacco@tecsup.edu.pe>
2025-04-09 14:37:57 -0500

    model registrado en admin.py

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Project, Task
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created_date')
+    search_fields = ('name', 'description')
+    list_filter = ('created_date',)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'priority', 'status', 'due_date')
+    search_fields = ('title', 'description')
+    list_filter = ('priority', 'status', 'due_date')
